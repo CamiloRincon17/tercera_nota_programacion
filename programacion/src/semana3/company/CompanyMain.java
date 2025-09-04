0eaeeb918afcb413eed224afdf63af435be16319
camilo <camilo171512@gmail.com>
2025-09-03 19:48:47 -0500

    se empezo proyecto EMPLOYEE

---
+++ programacion/src/semana3/company/CompanyMain.java
@@ -0,0 +1,7 @@
+package semana3.company.hr;
+
+public class companymain {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ programacion/src/semana3/company/Employee.java
@@ -0,0 +1,8 @@
+package semana3.company.hr;
+
+public class Employee {
+    public String name ;
+    private double salary;
+    protected String role;
+    
+}
